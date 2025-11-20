716393a7428406119cef75a040cbb6bef6977bfe
MAHY U DEEN SHAHID <mahyudeenshahidiqbal@gmail.com>
2025-11-21 02:37:32 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
   output: process.env.NEXT_OUTPUT_MODE,
   experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
+    outputFileTracingRoot: __dirname, // point to project root
   },
   eslint: {
     ignoreDuringBuilds: true,
